ffc63ef7ba5a1aec939c43c55aed2c792f453573
Chris Swithinbank <swithinbank@gmail.com>
2024-11-25 21:50:55 +0100

    Fix Tailwind

--- src/build.mjs
+++ src/build.mjs
@@ -4,10 +4,12 @@ import { build } from 'astro';
 import { createSpinner } from 'nanospinner';
 import fs from 'node:fs/promises';
 import path from 'node:path';
+import process from 'node:process';
+import { baseInject } from './base-integration/integration.mjs';
 import { downloadTemplates } from './download.mjs';
 import { cleanAndCreateDirectory } from './utils/cleanAndCreateDirectory.mjs';
-import { baseInject } from './base-integration/integration.mjs';
 
+const initialCWD = process.cwd();
 /** Directory to download Astro starter templates to. */
 const templateDir = '.templates';
 /** Directory to collect build output in for serving later. */
@@ -31,14 +33,17 @@ for (const dir of templates) {
 	const building = createSpinner(`Building ${dir.name}`).start();
 	const root = path.join(dir.parentPath, dir.name);
 	// Build example with Astro.
+	// We change CWD instead of using Astro’s `root` option because examples using Tailwind failed
+	// to find the Tailwind config file when using `root`.
+	process.chdir(root);
 	await build({
-		root,
 		outDir: './dist',
 		logLevel: 'error',
 		base: dir.name,
 		trailingSlash: 'always',
 		integrations: [baseInject({ base: dir.name })],
 	});
+	process.chdir(initialCWD);
 	// Move the build output to shared `dist/` directory.
 	fs.rename(path.join(root, 'dist'), path.join(buildDir, dir.name));
 	building.success();
